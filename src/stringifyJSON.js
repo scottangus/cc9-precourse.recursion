c0da47f4f453d68e3cc3ea1f7ad74f7b80da4ea7
Scott Angus <scottdangus@gmail.com>
2019-06-07 20:11:16 -0400

    Wrote recursive stringifyJSON function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,6 +6,33 @@ What does the output for stringifyJSON look like? Play around with JSON.stringif
 
 */
 
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (input) => {
+  var results = [];
+  //console.log(input);
+  //console.log(typeof input);
+  if (typeof input === "boolean" || typeof input === "number" || input === null){
+    return String(input);
+  }
+  else if (typeof input === "string"){
+    //console.log("I am a string");
+    return "\""+ input + "\"";
+  }
+  else if(Array.isArray(input)){
+    //console.log("I am an array");
+    for (var x = 0; x < input.length; x++){
+      results.push(stringifyJSON(input[x]));
+    }
+    return "[" + results + "]";
+  }
+  else if(typeof input === "object"){
+    //console.log("I am an object");
+    for (var currentKey in input){
+      if (input[currentKey] !== undefined && typeof input[currentKey] !== "function"){
+        results.push(stringifyJSON(currentKey) + ":" + stringifyJSON(input[currentKey]));
+      }
+    }
+    return "{" + results + "}"
+  }
+  return String(input);
+  //return String.toString(input);
 };
